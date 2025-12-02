cd9d78a63e71d08aaa0a61202d9efea609588590
kcsic <kcsic2000@gmail.com>
2025-12-02 14:36:59 +0900

    init

---
+++ src/main/java/com/snc/gift/SncApiGiftApplication.java
@@ -0,0 +1,13 @@
+package com.snc.gift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SncApiGiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SncApiGiftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/snc/gift/SncApiGiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snc.gift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SncApiGiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
