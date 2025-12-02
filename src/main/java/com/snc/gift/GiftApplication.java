54d73164ba6439904de70e16a78a5bf15aa7fe5f
kcsic <kcsic2000@gmail.com>
2025-12-02 14:43:46 +0900

    project refactoring

--- src/main/java/com/snc/gift/GiftApplication.java
+++ src/main/java/com/snc/gift/GiftApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SncApiGiftApplication {
+public class GiftApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SncApiGiftApplication.class, args);
+        SpringApplication.run(GiftApplication.class, args);
     }
 
 }
