1f08e5a7c93585916916264bf0795b2fb566370d
kcsic <kcsic2000@gmail.com>
2025-12-07 21:03:47 +0900

    swagger 주석

--- src/main/java/com/snc/gift/controller/TokenController.java
+++ src/main/java/com/snc/gift/controller/TokenController.java
@@ -3,13 +3,23 @@ package com.snc.gift.controller;
 import com.snc.gift.dto.response.TokenResponse;
 import com.snc.gift.service.TokenService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@Tag(name = "토큰", description = "토큰 관련 API")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "200" , description = "OK"),
+        @ApiResponse(responseCode = "500" , description = "Internal Server Error")
+})
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
