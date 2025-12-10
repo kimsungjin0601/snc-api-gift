ee507f94011fe44e9ef1822ea1345685f891d71a
kcsic <kcsic2000@gmail.com>
2025-12-11 01:42:30 +0900

    vo 추가

--- src/main/java/com/snc/gift/vo/PartnerVo.java
+++ src/main/java/com/snc/gift/vo/PartnerVo.java
@@ -11,14 +11,17 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 public class PartnerVo extends BaseVo {
 
+    @Schema(description="테넌트_번호")
+    private Long tenantNo;
+
     @Schema(description="파트너_번호")
     private Long partnerNo;
 
+    @Schema(description="파트너_코드")
+    private String partnerCode;
+
     @Schema(description="상위_파트너_번호")
     private Long parentPartnerNo;
-
-    @Schema(description="테넌트_번호")
-    private Long tenantNo;
 
     @Schema(description="도메인_번호")
     private Long partnerDomainNo;
