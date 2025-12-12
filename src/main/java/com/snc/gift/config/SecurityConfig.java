c6d42879b05637f4c429e1a45ea965f956af87e7
kcsic <kcsic2000@gmail.com>
2025-12-12 22:33:19 +0900

    config 수정

--- src/main/java/com/snc/gift/config/SecurityConfig.java
+++ src/main/java/com/snc/gift/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -34,27 +35,46 @@ public class SecurityConfig {
 	private final CustomAccessDeniedHandler customAccessDeniedHandler;
 	private final CustomAuthEntryPoint customAuthEntryPoint;
 
+	private static final String[] permitAllArray = {
+			"/", "/api-docs/**", "/swagger-ui/**",
+			"/api/auth/member/sign-in","/api/auth/admin/sign-in","/api/auth/purchase/sign-in",
+			"/api/auth/sign-in/**","/api/system/admin",
+			"/api/front/member/sign-up","/api/front/main","/api/front/gift/**"
+	};
+
 	@Bean
-    SecurityFilterChain filterChain(HttpSecurity http) {
+	@Order(1)
+	public SecurityFilterChain publicChain(HttpSecurity http) throws Exception {
 		http.cors(cors -> cors.configurationSource(corsConfigurationSource))
+			.securityMatcher(permitAllArray)
 			.csrf(AbstractHttpConfigurer::disable)
-			.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.formLogin(AbstractHttpConfigurer::disable)
 			.httpBasic(AbstractHttpConfigurer::disable)
+			.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+			.exceptionHandling(handler -> handler
+				.authenticationEntryPoint(customAuthEntryPoint)
+				.accessDeniedHandler(customAccessDeniedHandler));
+		return http.build();
+	}
+
+	@Bean
+	@Order(2)
+	public SecurityFilterChain securityChain(HttpSecurity http) throws Exception {
+		http.cors(cors -> cors.configurationSource(corsConfigurationSource))
+			.csrf(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(auth -> auth
-					.requestMatchers("/", "/api-docs/**", "/swagger-ui/**").permitAll()
-					.requestMatchers("/api/auth/member/sign-in","/api/auth/admin/sign-in","/api/auth/purchase/sign-in").permitAll()
-					.requestMatchers("/api/auth/sign-in/**","/api/system/admin").permitAll()
-					.requestMatchers("/api/front/member/sign-up","/api/front/main","/api/front/gift/**").permitAll()
-					.requestMatchers("/api/auth/sign-out","/api/auth/sign-out/force/**").permitAll()
-					.requestMatchers("/api/front/**").hasRole("MEMBER")
-					.requestMatchers("/api/admin/**").hasAnyRole("AGENT_ADMIN","HQ_ADMIN","SUPER_HQ_ADMIN","SYSTEM_ADMIN")
-					.requestMatchers("/api/purchase/**").hasRole("HQ_ORDER")
-					.anyRequest().permitAll()
+				.requestMatchers("/api/front/**").hasRole("MEMBER")
+				.requestMatchers("/api/admin/**").hasAnyRole("AGENT_ADMIN","HQ_ADMIN","SUPER_HQ_ADMIN","SYSTEM_ADMIN")
+				.requestMatchers("/api/purchase/**").hasRole("HQ_ORDER")
+				.anyRequest().authenticated()
 			)
 			.exceptionHandling(handler -> handler
-					.authenticationEntryPoint(customAuthEntryPoint)
-					.accessDeniedHandler(customAccessDeniedHandler))
+				.authenticationEntryPoint(customAuthEntryPoint)
+				.accessDeniedHandler(customAccessDeniedHandler))
 			.addFilterBefore(jwtAuthFilter(), UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
